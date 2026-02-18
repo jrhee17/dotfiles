a4dae7f9128f772d4463ca2702acc79e1c04397e
Hannam Rhee <guins_j@guins.org>
2026-02-18 14:29:04 +0000

    add nvim ref follow/reload behavior

--- remote-dev/.config/nvim/init.lua
+++ remote-dev/.config/nvim/init.lua
@@ -46,6 +46,11 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep)
 vim.keymap.set("n", "<leader>fb", builtin.buffers)
 -- help tags
 vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+-- reference
+vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions)
+vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations)
+vim.keymap.set("n", "<leader>gr", builtin.lsp_references)
+-- vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
 -- diagnostic
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
@@ -64,3 +69,9 @@ require("nvim-tree").setup({
 })
 vim.keymap.set("n", "<leader>fe", ":NvimTreeToggle<CR>")
 
+-- reload on file change
+vim.o.autoread = true
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  command = "checktime",
+})
