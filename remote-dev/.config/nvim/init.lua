5d404bbc18c61ba590b880d4489d01f1894933f1
Hannam Rhee <guins_j@guins.org>
2026-03-14 04:15:14 +0000

    add rename

--- remote-dev/.config/nvim/init.lua
+++ remote-dev/.config/nvim/init.lua
@@ -95,6 +95,7 @@ vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions)
 vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations)
 vim.keymap.set("n", "<leader>gr", builtin.lsp_references)
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
 -- diagnostic
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
